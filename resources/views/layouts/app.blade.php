88b07542888e1322185d69cecfd15e263e987344
SakaDevs <putusakaamerta@gmail.com>
2026-05-04 19:09:53 +0700

    UPDATE

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="Download script skin MLBB terbaru, gratis, dan aman.">
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=3">
         <title>{{ config('app.name', 'Laravel') }}</title>
 
